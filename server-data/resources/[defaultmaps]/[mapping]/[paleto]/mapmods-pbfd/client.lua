84df975fb6a8675b817f4dd38b1c527cac54dd54
loollein <shievchienkodanil2002@gmail.com>
2023-03-18 23:13:37 +0700

    [Con02] Added new interior for Paleto Fire Department

---
+++ server-data/resources/[defaultmaps]/[mapping]/[paleto]/mapmods-pbfd/client.lua
@@ -0,0 +1,3 @@
+Citizen.CreateThread(function()
+	LoadInterior(GetInteriorAtCoords(-370.52, 6105.49, 31.69))
+end)
---
+++ server-data/resources/[defaultmaps]/[mapping]/[paleto]/mapmods-pbfd/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+files {
+  'data/interiorproxies.meta'
+}
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "data/interiorproxies.meta"
+
+client_script 'client.lua'
