d1f3296841d4e4b81ab63b18658cd4a29f00a082
Ilya Bril <brilillja81@gmail.com>
2023-03-24 18:35:43 +0200

    [Con27] Updated Weather

--- server-data/resources/[qb]/qb-weathersync/config.lua
+++ server-data/resources/[qb]/qb-weathersync/config.lua
@@ -2,14 +2,14 @@ Config                  = {}
 Config.DynamicWeather   = true -- Set this to false if you don't want the weather to change automatically every 10 minutes.
 
 -- On server start
-Config.StartWeather     = 'XMAS' -- Default weather                       default: 'EXTRASUNNY'
-Config.BaseTime         = 8 -- Time                                             default: 8
-Config.TimeOffset       = 0 -- Time offset                                      default: 0
-Config.FreezeTime       = false -- freeze time                                  default: false
-Config.Blackout         = false -- Set blackout                                 default: false
-Config.BlackoutVehicle  = false -- Set blackout affects vehicles                default: false
-Config.NewWeatherTimer  = 10 -- Time (in minutes) between each weather change   default: 10
-Config.Disabled         = false -- Set weather disabled                         default: false
+Config.StartWeather     = 'FOGGY'    -- Default weather                       default: 'EXTRASUNNY'
+Config.BaseTime         = 8         -- Time                                          
+Config.TimeOffset       = 0         -- Time offset                                      
+Config.FreezeTime       = false     -- freeze time                                
+Config.Blackout         = false     -- Set blackout                               
+Config.BlackoutVehicle  = false     -- Set blackout affects vehicles              
+Config.NewWeatherTimer  = 5        -- Time (in minutes) between each weather change   
+Config.Disabled         = false     -- Set weather disabled                        
 
 
 Config.AvailableWeatherTypes = { -- DON'T TOUCH EXCEPT IF YOU KNOW WHAT YOU ARE DOING
