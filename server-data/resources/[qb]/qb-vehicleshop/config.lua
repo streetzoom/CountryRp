a5c9108020b3db014d802538ea2cd7be31a9bc0b
shumniy <shumniy1990@gmail.com>
2023-03-24 10:06:20 +0700

    [Con24] Relocate Air Shop

--- server-data/resources/[qb]/qb-vehicleshop/config.lua
+++ server-data/resources/[qb]/qb-vehicleshop/config.lua
@@ -190,13 +190,13 @@ Config.Shops = {
         ['Type'] = 'free-use', -- no player interaction is required to purchase a vehicle
         ['Zone'] = {
             ['Shape'] = {--polygon that surrounds the shop
-                vector2(-1607.58, -3141.7),
-                vector2(-1672.54, -3103.87),
-                vector2(-1703.49, -3158.02),
-                vector2(-1646.03, -3190.84)
+                vector2(1748.86, 3299.98),
+                vector2(1740.12, 3332.14),
+                vector2(1713.08, 3324.95),
+                vector2(1721.7, 3292.61)
             },
             ['minZ'] = 12.99, -- min height of the shop zone
-            ['maxZ'] = 16.99, -- max height of the shop zone
+            ['maxZ'] = 50.99, -- max height of the shop zone
             ['size'] = 7.0, -- size of the vehicles zones
         },
         ['Job'] = 'none', -- Name of job or none
@@ -205,28 +205,18 @@ Config.Shops = {
         ['blipSprite'] = 251, -- Blip sprite
         ['blipColor'] = 3, -- Blip color
         ['TestDriveTimeLimit'] = 1.5, -- Time in minutes until the vehicle gets deleted
-        ['Location'] = vector3(-1652.76, -3143.4, 13.99), -- Blip Location
-        ['ReturnLocation'] = vector3(-1628.44, -3104.7, 13.94), -- Location to return vehicle, only enables if the vehicleshop has a job owned
-        ['VehicleSpawn'] = vector4(-1617.49, -3086.17, 13.94, 329.2), -- Spawn location when vehicle is bought
-        ['TestDriveSpawn'] = vector4(-1625.19, -3103.47, 13.94, 330.28), -- Spawn location for test drive
+        ['Location'] = vector3(1731.32, 3309.39, 41.22), -- Blip Location
+        ['ReturnLocation'] = vector3(1731.32, 3309.39, 41.22), -- Location to return vehicle, only enables if the vehicleshop has a job owned
+        ['VehicleSpawn'] = vector4(1575.55, 3177.34, 40.53, 314.64), -- Spawn location when vehicle is bought
+        ['TestDriveSpawn'] = vector4(1700.68, 3249.32, 40.96, 103.24), -- Spawn location for test drive
         ['ShowroomVehicles'] = {
             [1] = {
-                coords = vector4(-1651.36, -3162.66, 12.99, 346.89), -- where the vehicle will spawn on display
-                defaultVehicle = 'volatus', -- Default display vehicle
-                chosenVehicle = 'volatus' -- Same as default but is dynamically changed when swapping vehicles
-            },
-            [2] = {
-                coords = vector4(-1668.53, -3152.56, 12.99, 303.22),
+                coords = vector4(1735.41, 3313.02, 41.22, 161.33),
                 defaultVehicle = 'luxor2',
                 chosenVehicle = 'luxor2'
             },
-            [3] = {
-                coords = vector4(-1632.02, -3144.48, 12.99, 31.08),
-                defaultVehicle = 'nimbus',
-                chosenVehicle = 'nimbus'
-            },
-            [4] = {
-                coords = vector4(-1663.74, -3126.32, 12.99, 275.03),
+            [2] = {
+                coords = vector4(1726.52, 3304.45, 41.22, 242.3),
                 defaultVehicle = 'frogger',
                 chosenVehicle = 'frogger'
             },
